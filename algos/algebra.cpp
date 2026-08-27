6a920bd8806f288fae5c2e9b4905c88908212dca
Icy-13 <zorinms@mail.ru>
2026-08-27 18:21:06 +0300

    simplified chinese remainder theorem

--- algos/algebra.cpp
+++ algos/algebra.cpp
@@ -130,25 +130,21 @@ vector<int> multiply(vector<int> const& a, vector<int> const& b) {
     return fa;
 }
 
-// CRT
-struct Congruence {
-    long long a, m; // x = a (mod m)
-};
-
-long long chinese_remainder_theorem(vector<Congruence> const& congruences) {
+// КТО
+long long chinese_remainder_theorem(vector<pll> const& congruences) {
     // mods are coprime. Otherwise we either have one solution modulo lcm, or we don't have any
     // transofrm (a, m) congruence to (a, p1^n1) .. (a, pm^nm) congr, where p's are prime
     // if we have a contradiction, i.e. (1, 4) and (1, 2), then there's no sol, otherwise use default algo
     long long M = 1;
-    for (auto const& congruence : congruences) {
-        M *= congruence.m;
+    for (auto const& [a, m] : congruences) {
+        M *= m; // x = a (mod m)
     }
 
     long long solution = 0;
-    for (auto const& congruence : congruences) {
-        long long a_i = congruence.a;
-        long long M_i = M / congruence.m;
-        long long N_i = mod_inv(M_i, congruence.m);
+    for (auto const& [a, m] : congruences) {
+        long long a_i = a;
+        long long M_i = M / m;
+        long long N_i = mod_inv(M_i, m);
         solution = (solution + a_i * M_i % M * N_i) % M;
     }
     return solution;
