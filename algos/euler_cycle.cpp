b528724178b8520af6279a567124d2ddad093776
markinaa <annie.markina@gmail.com>
2026-09-23 17:54:53 +0300

    shortened

--- algos/euler_cycle.cpp
+++ algos/euler_cycle.cpp
@@ -1,30 +1,20 @@
-struct edge {
-    int vertex_to;
-    int edge_num;
-};
-
-//returns sequence of vertices representing euler path
-vector <int> euler_cycle(int start, int finish, vector <vector <edge> > &g, int edges_num, int n) {
-    vector <bool> used(edges_num);
-    vector <int> path, vec;
-    vector <int> it(n);
-    vec.push_back(start);
+vector<int> euler_cycle(int start, vector<vector<pii>> &g, int m) {
+    vector<bool> used(m);
+    vector<int> it(g.size()), path, vec = {start};
     while (!vec.empty()) {
         int cur = vec.back();
         while (it[cur] < g[cur].size()) {
-            edge to = g[cur][it[cur]];
-            if (!used[to.edge_num]) break;
-            else it[cur]++;
+            if (!used[g[cur][it[cur]].second]) break;
+            it[cur]++;
         }
         if (it[cur] >= g[cur].size()) {
             path.push_back(cur);
             vec.pop_back();
         } else {
-            edge to = g[cur][it[cur]];
-            vec.push_back(to.vertex_to);
-            used[to.edge_num] = 1;
+            vec.push_back(g[cur][it[cur]].first);
+            used[g[cur][it[cur]].second] = 1;
         }
     }
-    reverse(path.begin(), path.end());
+    reverse(all(path));
     return path;
 }
